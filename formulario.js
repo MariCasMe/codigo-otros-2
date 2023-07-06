d0335f961d3dbc81ddc59e850b773fc49f67ee93
MariCasMe <mariadejesus.cmendoza@gmail.com>
2023-07-05 22:06:14 -0600

    Resuelto

---
+++ formulario.js
@@ -0,0 +1,89 @@
+var formulario = document.querySelector(".formulario");
+
+formulario.onsubmit = function(e) {
+
+  e.preventDefault();
+  
+  var nameInput= formulario.elements.name;
+  var ageInput= formulario.elements.age;
+  var nationInput = formulario.elements.nationality;
+
+  var nombre = nameInput.value;
+  var edad = ageInput.value;
+
+  var nation = nationInput.selectedIndex;
+  var nacionalidad = nationInput.options[nation].value;
+  console.log(nombre, edad);
+  console.log(nacionalidad);
+
+  if (nombre.length === 0) {
+    nameInput.classList.add("error");
+  }
+  if (edad < 18 || edad > 120) {
+    ageInput.classList.add("error");
+  }
+
+if (nombre.length > 0 
+  && (edad > 18 
+    && edad < 120) ) {
+  agregarInvitado(nombre, edad, nacionalidad);
+  }
+}
+
+function agregarInvitado(nombre, edad, nacionalidad) {
+
+  if (nacionalidad === "ar") {
+    nacionalidad = "Argentina";
+  }
+  else if (nacionalidad === "mx") {
+    nacionalidad = "Mexicana";
+  }
+  else if (nacionalidad === "vnzl") {
+    nacionalidad = "Venezolana";
+  }
+  else if (nacionalidad === "per") {
+    nacionalidad = "Peruana";
+  }
+
+  var lista = document.getElementById("lista-de-invitados");
+
+  var elementoLista = document.createElement("div");
+  elementoLista.classList.add("elemento-lista");
+  lista.appendChild(elementoLista);
+
+  var spanNombre = document.createElement("span");
+  var inputNombre = document.createElement("input");
+  var espacio = document.createElement("br");
+  spanNombre.textContent = "Nombre: ";
+  elementoLista.appendChild(spanNombre);
+  elementoLista.appendChild(inputNombre);
+  elementoLista.appendChild(espacio);
+
+  function crearElemento(descripcion, valor) {
+    var spanNombre = document.createElement("span");
+    var inputNombre = document.createElement("input");
+    var espacio = document.createElement("br");
+    spanNombre.textContent = descripcion + ": ";
+    inputNombre.value = valor ;
+    elementoLista.appendChild(spanNombre);
+    elementoLista.appendChild(inputNombre);
+    elementoLista.appendChild(espacio);
+  }
+
+  crearElemento("Nombre", nombre);
+  crearElemento("Edad", edad);
+  crearElemento("Nacionalidad", nacionalidad);
+
+
+  var botonBorrar = document.createElement("button");
+  botonBorrar.textContent = "Eliminar invitado";
+  botonBorrar.id = "boton-borrar";
+  var corteLinea = document.createElement("br");
+  elementoLista.appendChild(corteLinea);
+  elementoLista.appendChild(botonBorrar);
+
+  botonBorrar.onclick = function() {
+    // this.parentNode.style.display = 'none';
+    botonBorrar.parentNode.remove()
+  }
+}
